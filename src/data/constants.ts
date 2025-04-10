c58006670ce55e9e62cac982153cc9ee84bd26b1
Marco Tortolani <mjtorto@gmail.com>
2025-04-10 14:28:19 -0300

    updates

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,4 +1,4 @@
-const ENDPOINT_BASE = 'https://test.api.gaming.moob.club/api/v1/'
+const ENDPOINT_BASE = 'https://api.gaming.moob.club/api/v1/'
 
 export const ENDPOINT_CONFIG = ENDPOINT_BASE + 'getTrivia'
 export const UPDATE_SCORE_ENDPOINT = ENDPOINT_BASE + 'updatescore/'
